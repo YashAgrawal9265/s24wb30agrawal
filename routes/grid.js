eae81b817850edf8ddb125101b602540a14c9866
Yash Agrawal <yashagrawal9265@gmail.com>
2024-03-26 16:25:59 -0500

    added grid boxes

--- routes/grid.js
+++ routes/grid.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET grid page. */
 router.get('/', function(req, res, next) {
   let query = req.query
   console.log(`rows ${query.rows}`)
--- routes/upliftedartwork.js
+++ routes/upliftedartwork.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET upliftedartwork page. */
 router.get('/', function(req, res, next) {
   res.render('upliftedartwork', { title: 'Search Results - Uplifted Artwork' });
 });
