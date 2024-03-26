3079c5619f4a4ebf954d71af623b9c8f9a3b4820
Yash Agrawal <yashagrawal9265@gmail.com>
2024-03-26 13:17:24 -0500

    added upliftedartwork route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var upliftedartworkRouter = require('./routes/upliftedartwork');
 
 var app = express();
 
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/upliftedartwork', upliftedartworkRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
